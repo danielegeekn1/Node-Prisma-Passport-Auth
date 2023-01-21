41d1ba4972c4fb1d8426ac32c900c605dad87855
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-21 15:28:19 +0100

    added some routes

--- server.ts
+++ server.ts
@@ -5,16 +5,23 @@ const prisma = new PrismaClient();
 const port = process.env.PORT;
 router.use(express.json());
 router.set("view engine", "ejs");
-/*
-router.get("/users", async (req, res) => {
+
+router.get("/", (req, res) => {
+  res.send("home");
+});
+router.get("/login", async (req, res) => {
   try {
     const user = await prisma.users.findMany();
+    res.render("login");
     res.json(user);
   } catch (error) {
     console.error(error);
   }
 });
-router.post("/users/register", async (req: Request, res: Response) => {
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
   const createUsers = await prisma.users.create({
     data: {
@@ -24,16 +31,6 @@ router.post("/users/register", async (req: Request, res: Response) => {
     },
   });
   res.status(200).json(createUsers);
-});
-*/
-router.get("/", (req, res) => {
-  res.send("home");
-});
-router.get("/login", (req, res) => {
-  res.render("login");
-});
-router.get("/register", (req, res) => {
-  res.render("register");
 });
 router.get("/logout", (req, res) => {
   res.render("logout");
