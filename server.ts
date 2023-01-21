9821884a1c169fc7b4841bc1417e7253fcd67a21
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-21 19:18:08 +0100

    configured passport

--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 import express, { NextFunction, Request, Response } from "express";
 import { CustomError } from "./custom-error/custom-error";
 import { router as usersRouter } from "./routes/routes";
+import { initSessionMiddleware } from "./passport-config/session";
+import { passport } from "./passport-config/passportConfig";
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
 app.use("/", usersRouter);
+//init passport middleware
+app.use(initSessionMiddleware());
+
+//init passport
+app.use(passport.initialize());
+//init passport session middleware
+app.use(passport.session());
 //check for 404 error that has to work globally
 app.use(() => {
   throw new CustomError("Could not find route", 404);
