484109041af4dec9e51d4000a7e13014deb16e2c
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-21 15:23:43 +0100

    added some style to ejs files form

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const router = express();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const port = process.env.PORT;
 router.use(express.json());
-router.set("view", "ejs");
+router.set("view engine", "ejs");
+/*
 router.get("/users", async (req, res) => {
   try {
     const user = await prisma.users.findMany();
@@ -12,6 +13,30 @@ router.get("/users", async (req, res) => {
   } catch (error) {
     console.error(error);
   }
+});
+router.post("/users/register", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const createUsers = await prisma.users.create({
+    data: {
+      name: name,
+      email: email,
+      password: password,
+    },
+  });
+  res.status(200).json(createUsers);
+});
+*/
+router.get("/", (req, res) => {
+  res.send("home");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.get("/logout", (req, res) => {
+  res.render("logout");
 });
 router.listen(port, () => {
   console.log(`SERVER: running on port ${port}`);
